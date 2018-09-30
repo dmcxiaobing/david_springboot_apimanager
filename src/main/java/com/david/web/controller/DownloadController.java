979d432316367e7a33df4e83ad7d36d5eb65108f
David <986945193@qq.com>
2018-09-30 15:34:39 +0800

    download file

--- src/main/java/com/david/web/controller/DownloadController.java
+++ src/main/java/com/david/web/controller/DownloadController.java
@@ -1,11 +1,13 @@
 package com.david.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import sun.misc.BASE64Encoder;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URLEncoder;
 
 @RequestMapping("/download")
 @RestController
@@ -15,7 +17,7 @@ public class DownloadController {
         File file = new File("D:/logs/warn.log");
         resp.setHeader("content-type", "application/octet-stream");
         resp.setContentType("application/octet-stream");
-        resp.setHeader("Content-Disposition", "attachment;filename=" + "abc.log");
+        resp.setHeader("Content-Disposition", "attachment;filename=" + "warn.log");
         byte[] buff = new byte[1024];
         BufferedInputStream bis = null;
         OutputStream os = null;
@@ -34,6 +36,63 @@ public class DownloadController {
             if (bis != null) {
                 try {
                     bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @RequestMapping(value = "/testDownload2")
+    public void testDownload2(HttpServletResponse response, HttpServletRequest request) throws IOException {
+        File file = new File("D:/logs/日志副本.log");
+
+        String filename = "日志副本.log";
+
+        //获得请求头中的User-Agent
+        String agent = request.getHeader("User-Agent");
+        //根据不同浏览器进行不同的编码
+        String filenameEncoder = "";
+        if (agent.contains("MSIE")) {
+            // IE浏览器
+            filenameEncoder = URLEncoder.encode(filename, "utf-8");
+            filenameEncoder = filenameEncoder.replace("+", " ");
+        } else if (agent.contains("Firefox")) {
+            // 火狐浏览器
+            BASE64Encoder base64Encoder = new BASE64Encoder();
+            filenameEncoder = "=?utf-8?B?"
+                    + base64Encoder.encode(filename.getBytes("utf-8")) + "?=";
+        } else {
+            // 其它浏览器
+            filenameEncoder = URLEncoder.encode(filename, "utf-8");
+        }
+
+        //要下载的文件类型----客户端通过文件的MINE类型区分类型
+        String mimeType = request.getServletContext().getMimeType(filename);
+
+        response.setContentType(mimeType);
+        //告诉客户端该文件不是直接解析而是以附件形式打开(下载)
+        response.setHeader("Content-Disposition", "attachment;filename=" + filenameEncoder);
+
+        byte[] buff = new byte[1024];
+        BufferedInputStream bis = null;
+        OutputStream os = null;
+        try {
+            os = response.getOutputStream();
+            bis = new BufferedInputStream(new FileInputStream(file));
+            int i = bis.read(buff);
+            while (i != -1) {
+                os.write(buff, 0, buff.length);
+                os.flush();
+                i = bis.read(buff);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                    bis = null;
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
